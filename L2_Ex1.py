af23406c46aac60542ed02712cd6f49d2cf1f3d3
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-11-10 09:37:15 +1000

    stuff

--- L2_Ex1.py
+++ L2_Ex1.py
@@ -4,8 +4,12 @@ window = turtle.Screen()
 window.setup(500, 500)
 my_ttl = turtle.Turtle()
 
+
 ##############################
 ## Draw a square in 3 lines ##
 ##############################
 
 
+for sides in range(200):
+    my_ttl.forward(sides)
+    my_ttl.right(20)
