e50e925be1c636d850988739e5a6d22340db15e6
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2022-11-17 19:49:31 +1000

    Update deleteme.py

--- deleteme.py
+++ deleteme.py
@@ -1,24 +1,4 @@
-import random
-
-def get_number(prompt):
-    num = input(prompt)
-    if num.isdigit():
-        return int(num)
-    else:
-        print("Invalid input")
-        quit()
-
-number = random.randint(1,100)
-guess = 0
-
-while guess != number:
-    guess = get_number("Guess a number between 1 and 100> ")
-    
-    if guess > number:
-        print("Lower")
-    elif guess < number:
-        print("Higher")
-    else:
-        print("Correct")
-
-print("The number was", number)
+for num in range(10):
+    for num_2 in range(10):
+        for num_3 in range(10):
+            print(num, num_2, num_3)
